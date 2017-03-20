34c9f1501a1c7ab40dda6c6499325bc26f487fec
NoelMacwan <yulespencer@gmail.com>
2017-03-20 18:37:47 +0530

    shamu: rebrand to GlazeOS

--- glaze.mk
+++ glaze.mk
@@ -2,16 +2,16 @@
 TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common glaze stuff.
+$(call inherit-product, vendor/glaze/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/moto/shamu/aosp_shamu.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/moto/shamu/overlay-cm
+#DEVICE_PACKAGE_OVERLAYS += device/moto/shamu/overlay-cm
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_shamu
+PRODUCT_NAME := glaze_shamu
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 6
 
